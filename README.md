c108a2a827f5767895c87ac187f6e87babfe586d
John Laffey <laffeychem@gmail.com>
2022-11-30 19:16:24 -0500

    made game for guess number to learn dom manipulation

---
+++ README.md
@@ -0,0 +1,11 @@
+# Guess The Number
+
+---
+
+_This game was made simply to learn how to interact with the DOM. It works but doesn't store anything to a database so upon refresh the high score is gone._
+
+#### How to Play ~>
+
+1. Enter your guess in the box and follow the instructions.
+
+2. If you guess the number or lose, click the again button to play again.
---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+// Generate the number
+const genNumber = () => {
+  return Math.floor(Math.random() * 20 + 1);
+};
+// Assign number to variable
+let secretNumber = genNumber();
+console.log(secretNumber);
+
+let scoreCount = 20;
+
+// Game logic for the check button.
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (scoreCount === 1 && guess != secretNumber) {
+    document.body.style.backgroundColor = '#f03e3e';
+    scoreCount = 0;
+    document.querySelector('.score').textContent = '0';
+    document.querySelector('.message').textContent = 'You Lose! Try again!';
+  } else if (guess === secretNumber && scoreCount > 0) {
+    document.querySelector('.message').textContent = 'You Win!';
+    document.body.style.backgroundColor = '#60b347';
+    document.querySelector('.highscore').textContent =
+      document.querySelector('.score').textContent;
+  } else if (guess < secretNumber && scoreCount > 0) {
+    scoreCount--;
+    document.querySelector('.message').textContent = 'Too low!';
+    document.querySelector('.score').textContent = String(scoreCount);
+  } else if (guess > secretNumber && scoreCount > 0) {
+    scoreCount--;
+    document.querySelector('.message').textContent = 'Too high!';
+    document.querySelector('.score').textContent = String(scoreCount);
+  }
+});
+
+// Logic for the again button.
+
+document.querySelector('.again').addEventListener('click', () => {
+  let highScore = document.querySelector('.highscore').textContent;
+  //   Reset everything except the high score.
+  if (scoreCount > highScore) {
+    highScore = scoreCount;
+  }
+  document.body.style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').textContent = '';
+
+  document.querySelector('.score').textContent = 20;
+  scoreCount = 20;
+  secretNumber = genNumber();
+  return console.log(secretNumber), scoreCount;
+});
